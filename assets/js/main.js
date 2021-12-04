8c3c4db3e37325d862a2a3dc105d658c2804f917
Thảo Hà <thaoanhhaa1@gmail.com>
2021-12-04 22:37:11 +0700

    Respon Tablet

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,11 +10,13 @@ const loginFormBtn = document.querySelector('.header__form--login');
 const registerFormBtn = document.querySelector('.header__form--register');
 const form = document.querySelector('.form');
 const inputElements = document.querySelectorAll('.body-form input');
+const backToTopBtn = document.querySelector('.back-to-top');
+const barsBtn = document.querySelector('.heading__bars');
+const headingMenu = document.querySelector('.heading__menu');
 const KEY_USERS = JSON.parse(localStorage.getItem('KEY_USERS')) || [];
 
 var isFormLogin;
 var showPassword = false;
-var checkMenuUp = 0;
 const iconsInput = {
     user: '<i class="fas fa-user"></i>',
     password: '<i class="fas fa-unlock"></i>',
@@ -22,6 +24,17 @@ const iconsInput = {
     eyeShow: '<i class="show far fa-eye"></i>',
     eyeHidden: '<i class="hidden far fa-eye-slash"></i>'
 };
+
+
+/* Start Handler Margin-top: Attribute: margin-top */
+
+document.querySelectorAll('[margin-top]').forEach(element =>
+    element.style.marginTop = element.getAttribute('margin-top') + 'px');
+
+document.querySelectorAll('[margin-bottom]').forEach(element =>
+    element.style.marginBottom = element.getAttribute('margin-bottom') + 'px');
+
+/* End Handler Margin-top: Attribute: margin-top */
 
 /* Bắt đầu lắng nghe sự kiện click search btn */
 
@@ -40,18 +53,18 @@ function handelLoginForm() {
         <div class="error-messenger"></div>
         <div class="file-group">
             <label for="">
-                document.querySelector{iconsInput.mail}
+                ${iconsInput.mail}
             </label>
             <input type="text" name="username" required placeholder="Email or Username" class="">
         </div>
         <div class="file-group">
             <label for="">
-                document.querySelector{iconsInput.password}
+                ${iconsInput.password}
             </label>
             <input type="password" name="password" autocomplete required placeholder="Password" class="">
             <div class="eye">
-                document.querySelector{iconsInput.eyeShow}
-                document.querySelector{iconsInput.eyeHidden}
+                ${iconsInput.eyeShow}
+                ${iconsInput.eyeHidden}
             </div>
         </div>
         <div class="file-group">
@@ -75,28 +88,28 @@ function handelRegisterForm() {
         <div class="error-messenger"></div>
         <div class="file-group">
             <label for="">
-                document.querySelector{iconsInput.user}
+                ${iconsInput.user}
             </label>
             <input type="text" name="username" required placeholder="Username*" class="">
         </div>
 
         <div class="file-group half gap">
             <label for="">
-                document.querySelector{iconsInput.user}
+                ${iconsInput.user}
             </label>
             <input type="text" name="firstName" required placeholder="First name*" class="">
         </div>
 
         <div class="file-group half gap">
             <label for="">
-                document.querySelector{iconsInput.user}
+                ${iconsInput.user}
             </label>
             <input type="text" name="lastName" required placeholder="Last name" class="">
         </div>
 
         <div class="file-group">
             <label for="">
-                document.querySelector{iconsInput.mail}
+                ${iconsInput.mail}
             </label>
             <input type="email" name="email" required placeholder="Email*" class="">
         </div>
@@ -122,7 +135,7 @@ function handelCloseForm() {
 
 loginBtn.addEventListener('click', handelLoginForm)
 
-registerBtn.addEventListener('click', handelRegisterForm)
+registerBtn.addEventListener('click', handelRegisterForm);
 
 form.addEventListener('click', (e) => {
     if (e.target.closest('.eye')) {
@@ -142,7 +155,7 @@ form.addEventListener('click', (e) => {
     e.stopPropagation();
 })
 
-modalForm.addEventListener('click', handelCloseForm)
+modalForm.addEventListener('click', handelCloseForm);
 
 inputElements.forEach(inputElement => {
     inputElement.addEventListener('input', () => {
@@ -172,7 +185,7 @@ function handelLogin() {
 
 function innerHTMLElement(element, htmlInner) {
     element.style.display = 'block';
-    element.innerHTML = `document.querySelector{htmlInner}`;
+    element.innerHTML = `${htmlInner}`;
 }
 
 formElement.onsubmit = (e) => {
@@ -237,39 +250,108 @@ logoutBtn.addEventListener('click', handelLogout)
 
 /* Start Handel Navbar */
 
-function handelScrollHeadingMenu(element, obj) {
-    Object.assign(element.style, obj);
-}
+jQuery.fn.animateAuto = function(prop, speed, callback) {
+    var elem, height, width;
 
-window.onscroll = function() {
-    var scrollTop = window.scrollY || document.documentElement.scrollTop;
-    var headingMenu = document.querySelector('.heading__menu');
-    if (scrollTop > 500) {
-        checkMenuUp++;
-        if (!checkMenuUp) {
-            headingMenu.classList.add('animation-show');
-            setTimeout(() => headingMenu.classList.remove('animation-show'), 1000)
-        }
-        handelScrollHeadingMenu(headingMenu, {
-            'position': 'fixed',
-            'top': '0',
-            'left': '0',
-            'right': '0',
-            'z-index': '2',
-            'box-shadow': '0 0 5px rgb(0 0 0 / 40%)',
-            'background-color': '#fff',
-        })
-    } else {
-        checkMenuUp = 0;
-        handelScrollHeadingMenu(headingMenu, {
-            'display': 'flex',
-            'justify-content': 'center',
-            'align-items': 'center',
-            'height': '84px',
-            'position': 'relative',
-            'box-shadow': 'unset',
-        })
+    return this.each(function(i, el) {
+        el = jQuery(el), elem = el.clone().css({
+            "height": "auto",
+            "width": "auto"
+        }).appendTo("body");
+        height = elem.css("height"),
+            width = elem.css("width"),
+            elem.remove();
+
+        if (prop === "height")
+            el.animate({
+                "height": height
+            }, speed, callback);
+        else if (prop === "width")
+            el.animate({
+                "width": width
+            }, speed, callback);
+        else if (prop === "both")
+            el.animate({
+                "width": width,
+                "height": height
+            }, speed, callback);
+    });
+};
+
+document.querySelector('.heading__menu-sub-item').onclick = e => {
+    e.stopPropagation();
+};
+
+$(".heading__bars").bind("click", function(e) {
+    if (window.screen.availWidth <= 1024)
+        e.preventDefault();
+    document.querySelector('.heading__menu').classList.toggle('active');
+    if (document.querySelector('.heading__menu').classList.contains('active'))
+        $(".heading__menu").animateAuto("height", 200);
+    else
+        document.querySelector('.heading__menu').style.height = 0;
+});
+
+$(".sub__item").bind("click", function(e) {
+    var subElement = this.querySelector('.sub__item-list');
+    if (subElement) {
+        e.stopPropagation();
+        e.preventDefault();
+        subElement.classList.toggle('active');
+        if (subElement.classList.contains('active')) {
+            document.querySelector('.heading__menu').style.height = 'unset';
+            document.querySelector('.sub__menu').style.height = 'unset';
+            document.querySelector('.heading__menu-item').style.height = 'unset';
+            $(`.${Array.from(subElement.classList).join('.')}`).animateAuto("height", 200);
+        } else
+            subElement.style.height = 0;
     }
+});
+
+$(".heading__menu-item").bind("click", function(e) {
+    var subElement = this.querySelector('.sub__menu') || this.querySelector('.heading__menu-sub-item');
+    if (subElement) {
+        subElement.classList.toggle('active');
+        if (subElement.classList.contains('active')) {
+            document.querySelector('.heading__menu').style.height = 'unset';
+            $(`.${Array.from(subElement.classList).join('.')}`).animateAuto("height", 200);
+        } else
+            subElement.style.height = 0;
+        e.stopPropagation();
+        e.preventDefault();
+    }
+});
+
+// barsBtn.addEventListener('click', () => {
+//     // if (headingMenu.classList.contains('active'))
+//     //     headingMenu.style.height = '0';
+//     // else
+//     //     headingMenu.style.height = '100%';
+//     headingMenu.classList.toggle('active');
+// });
+
+window.onscroll = function(e) {
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (window.innerWidth > 1024) {
+        if (scrollTop > 500) {
+            if (!headingMenu.classList.contains('scroll')) {
+                headingMenu.classList.add('animation-show');
+                setTimeout(() => headingMenu.classList.remove('animation-show'), 1000)
+            }
+            headingMenu.classList.add('scroll');
+        } else
+            headingMenu.classList.remove('scroll');
+    } else {
+        if (document.querySelector('.heading__menu.scroll'))
+            headingMenu.classList.remove('scroll');
+    }
+
+
+    /* Back top top */
+    if (scrollTop > 500)
+        backToTopBtn.classList.add('active');
+    else
+        backToTopBtn.classList.remove('active');
 };
 
 /* End Handel Navbar */
@@ -284,27 +366,170 @@ $(document).ready(function() {
         touchThreshold: 99,
         speed: 500,
     });
+    $('.quick-take__list').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-arrow-left"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fas fa-arrow-right"></i></button>',
+        touchThreshold: 99,
+        speed: 500,
+        responsive: [{
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                }
+            },
+            {
+                breakpoint: 1023,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false,
+                }
+            },
+        ],
+    });
+    $('.slick-slider__col5').slick({
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+        arrows: false,
+        touchThreshold: 99,
+        // speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        swipeToSlide: true,
+        infinite: true,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 });
 
 /* End Slider */
 
-/* Start Selected */
+/* Start BackgroundImage */
 
-function setImageSelected() {
-    var srcImgs = [
-        '/assets/img/othxsvthtrc-555x360.jpg',
-        '/assets/img/story2.jpg',
-        '/assets/img/story3.jpg',
-    ];
-    document.querySelectorAll('.selected__item .content-img--link').forEach((element, index) => {
-        element.style.backgroundImage = `url('document.querySelector{srcImgs[index]}')`;
-    })
+function setImageBackground(selector, arrayImage) {
+    document.querySelectorAll(selector).forEach((element, index) =>
+        element.style.backgroundImage = `url('/assets/img/${arrayImage[index]}')`);
 }
 
-setImageSelected();
+function setBackgroundImage() {
+    var srcImgsLeft = [
+        'othxsvthtrc-555x360.jpg',
+        'story2.jpg',
+        'story3.jpg',
+    ];
+    setImageBackground('.selected__item .content-img--link', srcImgsLeft);
+    var srcImgsRight = [
+        'story4.jpg',
+        'european-news1.jpg',
+    ];
+    setImageBackground('.selected__item-img', srcImgsRight);
+    var imageSlickSliders = [
+        'cmpce1fznue-555x360.jpg',
+        'inhype2-photos-1-555x360.jpg',
+        'g0ocjglcehu-555x360.jpg',
+    ];
+    setImageBackground('.quick-take__item-img', imageSlickSliders);
+    var imageWorldWide = [
+        'cmpce1fznue-555x360.jpg',
+        'story2.jpg',
+        'story4.jpg',
+        'european-news1.jpg',
+        'oops1oswe0m-1140x694.jpg',
+        'inhype1-photos-3-1140x694.jpg',
+    ];
+    setImageBackground('.world-wide__item', imageWorldWide);
+    var imagePosts = [
+        'cmpce1fznue-555x360.jpg',
+        'inhype2-photos-1-555x360.jpg',
+        'othxsvthtrc-555x360.jpg',
+        '5ddyhjk_kmu-555x360.jpg',
+        'story2.jpg',
+        'story3.jpg',
+        'story4.jpg',
+    ];
+    setImageBackground('.posts__item .content-img--link', imagePosts);
+    var imageCategories = [
+        'whwybmtn3_0.jpg',
+        'fmnti8haab8.jpg',
+        'story2.jpg',
+        'cmpce1fznue-555x360.jpg',
+        'abi1mynlbga.jpg',
+    ];
+    setImageBackground('.categories__item', imageCategories);
+    var fixedIncome = [
+        'g0ocjglcehu-555x360.jpg',
+        'inhype2-photos-1-555x360.jpg',
+        'cmpce1fznue-555x360.jpg',
+        'othxsvthtrc-555x360.jpg',
+        'story3.jpg',
+        'story2.jpg',
+    ];
+    setImageBackground('.fixed-income .content-img--link', fixedIncome);
+}
 
-/* End Selected */
+setBackgroundImage();
 
-// document.querySelector(document).ready(function () {
-//     document.querySelector('.slider-list').slick();
-// });
+/* End BackgroundImage */
+
+/* Start Scroll Reveal */
+
+ScrollReveal({
+    reset: false,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+ScrollReveal().reveal('.content', {
+    delay: 500,
+    origin: 'bottom'
+});
+ScrollReveal().reveal('.content .content-list', {
+    delay: 600,
+    origin: 'bottom',
+    interval: 200
+});
+
+/* End Scroll Reveal */
+
+// Handel event click on back to top
+backToTopBtn.addEventListener('click', () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+});
+
+// Resize width
+window.onresize = () => {
+    var worldWideElement = document.querySelector('.world-wide');
+    if (window.innerWidth < 1024) {
+        worldWideElement.style.marginTop = '2px';
+    } else {
+        worldWideElement.style.marginTop =
+            `${worldWideElement.getAttribute('margin-top')}px`;
+    }
+};
